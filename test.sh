95c62773eda3a3877b3537fd578dd3b261f801bb
Christopher D. (Chris) Yi <115902339+mhsr21@users.noreply.github.com>
2025-01-28 14:09:13 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash scripts/train_llama_cora.sh 8 && bash scripts/test_llama_cora.sh 8
+bash scripts/train_llama_cora.sh 8 && bash scripts/test_llama_cora.sh 8 && bash scripts/test_llama_cora_link.sh 8
